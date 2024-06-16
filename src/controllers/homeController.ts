66d9ec0983d2d4e3c22caa7f24d8ab3c5da4b4c0
Thiago Grosse <thi_g@live.com>
2024-06-16 19:35:28 -0300

    Adicionado ajustes de rota

--- src/controllers/homeController.ts
+++ src/controllers/homeController.ts
@@ -3,8 +3,14 @@ import { Request, Response } from "express";
 export const getIndex = (req: Request, res: Response): void => {
     const links = {
         farolImoveis: {
-            url: "/farol-imoveis",
+            url: "/imobiliaria-farol",
             method: "POST",
+            body: "url da página",
+        },
+        imobiliariaJoris: {
+            url: "/imobiliaria-joris",
+            method: "POST",
+            body: "url da página",
         },
     };
 
