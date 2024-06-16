0ec8b95bf0ca8dc576d77b4943ea5a5dbaf55ece
Thiago Grosse <thi_g@live.com>
2024-06-16 17:49:59 -0300

    Adicionado arquivo vercel.json

--- src/server.ts
+++ src/server.ts
@@ -14,6 +14,8 @@ app.use(express.static(path.join(__dirname, "../public")));
 
 app.use("/", router);
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
