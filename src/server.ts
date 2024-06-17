93978a68816e204ea47d6f43a21ab4e98cd65b2d
Thiago Grosse <thi_g@live.com>
2024-06-16 22:00:25 -0300

    Adicionado configurações de CORS

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from "express";
 import helmet from "helmet";
 import router from "./routes";
 import path from "path";
+import cors from "cors";
 
 const app = express();
 app.use(helmet());
@@ -11,6 +12,13 @@ app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "../public")));
+app.use(
+    cors({
+        origin: "*",
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+    })
+);
 
 app.use("/", router);
 
